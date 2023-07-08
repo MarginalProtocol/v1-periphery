2ce56080a61151046fb20a31a244a385ab969f0c
0xcivita <0xcivita@proton.me>
2023-07-08 18:29:32 -0400

    test router remove liquidity when reverts

--- tests/functional/router/test_router_remove_liquidity.py
+++ tests/functional/router/test_router_remove_liquidity.py
@@ -1,3 +1,6 @@
+from ape import reverts
+
+
 def test_remove_liquidity__updates_liquidity(
     pool_initialized_with_liquidity,
     router,
@@ -173,13 +176,102 @@ def test_remove_liquidity__deposits_weth():
     pass
 
 
-def test_remove_liquidity__reverts_when_past_deadline():
-    pass
+def test_remove_liquidity__reverts_when_past_deadline(
+    pool_initialized_with_liquidity,
+    router,
+    sender,
+    alice,
+    chain,
+):
+    shares_sender = pool_initialized_with_liquidity.balanceOf(sender.address)
+    shares = shares_sender // 2
+    amount0_min = 0
+    amount1_min = 0
+    deadline = chain.pending_timestamp - 1
+    params = (
+        pool_initialized_with_liquidity.token0(),
+        pool_initialized_with_liquidity.token1(),
+        pool_initialized_with_liquidity.maintenance(),
+        alice.address,
+        shares,
+        amount0_min,
+        amount1_min,
+        deadline,
+    )
+
+    with reverts("Transaction too old"):
+        router.removeLiquidity(params, sender=sender)
 
 
-def test_remove_liquidity__reverts_when_amount0_less_than_min():
-    pass
+def test_remove_liquidity__reverts_when_amount0_less_than_min(
+    pool_initialized_with_liquidity,
+    router,
+    sender,
+    alice,
+    chain,
+    liquidity_math_lib,
+):
+    state = pool_initialized_with_liquidity.state()
+    shares_sender = pool_initialized_with_liquidity.balanceOf(sender.address)
+    total_shares = pool_initialized_with_liquidity.totalSupply()
+    total_liquidity = (
+        state.liquidity + pool_initialized_with_liquidity.liquidityLocked()
+    )
+
+    shares = shares_sender // 2
+    liquidity_delta = (total_liquidity * shares) // total_shares
+    amount0, amount1 = liquidity_math_lib.toAmounts(liquidity_delta, state.sqrtPriceX96)
+
+    amount0_min = amount0 + 1
+    amount1_min = 0
+    deadline = chain.pending_timestamp + 3600
+    params = (
+        pool_initialized_with_liquidity.token0(),
+        pool_initialized_with_liquidity.token1(),
+        pool_initialized_with_liquidity.maintenance(),
+        alice.address,
+        shares,
+        amount0_min,
+        amount1_min,
+        deadline,
+    )
+
+    with reverts(router.Amount0LessThanMin, amount0=amount0):
+        router.removeLiquidity(params, sender=sender)
 
 
-def test_remove_liquidity__reverts_when_amount1_less_than_min():
-    pass
+def test_remove_liquidity__reverts_when_amount1_less_than_min(
+    pool_initialized_with_liquidity,
+    router,
+    sender,
+    alice,
+    chain,
+    liquidity_math_lib,
+):
+    state = pool_initialized_with_liquidity.state()
+    shares_sender = pool_initialized_with_liquidity.balanceOf(sender.address)
+    total_shares = pool_initialized_with_liquidity.totalSupply()
+    total_liquidity = (
+        state.liquidity + pool_initialized_with_liquidity.liquidityLocked()
+    )
+
+    shares = shares_sender // 2
+    liquidity_delta = (total_liquidity * shares) // total_shares
+    amount0, amount1 = liquidity_math_lib.toAmounts(liquidity_delta, state.sqrtPriceX96)
+
+    amount0_min = 0
+    amount1_min = amount1 + 1
+    deadline = chain.pending_timestamp + 3600
+    params = (
+        pool_initialized_with_liquidity.token0(),
+        pool_initialized_with_liquidity.token1(),
+        pool_initialized_with_liquidity.maintenance(),
+        alice.address,
+        shares,
+        amount0_min,
+        amount1_min,
+        deadline,
+    )
+
+    with reverts(router.Amount1LessThanMin, amount1=amount1):
+        router.removeLiquidity(params, sender=sender)
