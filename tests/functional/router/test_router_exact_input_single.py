b396694ef7b462a31847a2581d3ecadd52a236dc
0xcivita <0xcivita@proton.me>
2023-07-12 10:07:54 -0400

    test remaining router exact input single

--- tests/functional/router/test_router_exact_input_single.py
+++ tests/functional/router/test_router_exact_input_single.py
@@ -1,5 +1,6 @@
 import pytest
 
+from ape import reverts
 from utils.utils import calc_amounts_from_liquidity_sqrt_price_x96
 
 
@@ -85,8 +86,105 @@ def test_router_exact_input_single__transfers_funds(
     alice,
     chain,
     zero_for_one,
+    token0,
+    token1,
+    sqrt_price_math_lib,
+    liquidity_math_lib,
+    swap_math_lib,
 ):
-    pass
+    state = pool_initialized_with_liquidity.state()
+    fee = pool_initialized_with_liquidity.fee()
+
+    token_in = (
+        pool_initialized_with_liquidity.token0()
+        if zero_for_one
+        else pool_initialized_with_liquidity.token1()
+    )
+    token_out = (
+        pool_initialized_with_liquidity.token1()
+        if zero_for_one
+        else pool_initialized_with_liquidity.token0()
+    )
+    maintenance = pool_initialized_with_liquidity.maintenance()
+
+    deadline = chain.pending_timestamp + 3600
+    amount_out_min = 0
+    sqrt_price_limit_x96 = 0
+
+    (reserve0, reserve1) = calc_amounts_from_liquidity_sqrt_price_x96(
+        state.liquidity, state.sqrtPriceX96
+    )
+    amount_in = 1 * reserve0 // 100 if zero_for_one else 1 * reserve1 // 100
+
+    # cache balances before swap
+    balance0_sender = token0.balanceOf(sender.address)
+    balance1_sender = token1.balanceOf(sender.address)
+
+    balance0_pool = token0.balanceOf(pool_initialized_with_liquidity.address)
+    balance1_pool = token1.balanceOf(pool_initialized_with_liquidity.address)
+
+    balance0_alice = token0.balanceOf(alice.address)
+    balance1_alice = token1.balanceOf(alice.address)
+
+    params = (
+        token_in,
+        token_out,
+        maintenance,
+        alice.address,  # recipient
+        deadline,
+        amount_in,
+        amount_out_min,
+        sqrt_price_limit_x96,
+    )
+    router.exactInputSingle(params, sender=sender)
+
+    # calculate amount out
+    amount_in_less_fee = amount_in - swap_math_lib.swapFees(amount_in, fee)
+    sqrt_price_x96_next = sqrt_price_math_lib.sqrtPriceX96NextSwap(
+        state.liquidity,
+        state.sqrtPriceX96,
+        zero_for_one,
+        amount_in_less_fee,
+    )  # price change before fees added
+
+    (amount0, amount1) = swap_math_lib.swapAmounts(
+        state.liquidity, state.sqrtPriceX96, sqrt_price_x96_next
+    )
+    amount_out = -amount1 if zero_for_one else -amount0
+
+    balance0_sender_after = (
+        balance0_sender - amount_in if zero_for_one else balance0_sender
+    )
+    balance1_sender_after = (
+        balance1_sender if zero_for_one else balance1_sender - amount_in
+    )
+
+    assert token0.balanceOf(sender.address) == balance0_sender_after
+    assert token1.balanceOf(sender.address) == balance1_sender_after
+
+    balance0_alice_after = (
+        balance0_alice if zero_for_one else balance0_alice + amount_out
+    )
+    balance1_alice_after = (
+        balance1_alice + amount_out if zero_for_one else balance1_alice
+    )
+
+    assert token0.balanceOf(alice.address) == balance0_alice_after
+    assert token1.balanceOf(alice.address) == balance1_alice_after
+
+    balance0_pool_after = (
+        balance0_pool + amount_in if zero_for_one else balance0_pool - amount_out
+    )
+    balance1_pool_after = (
+        balance1_pool - amount_out if zero_for_one else balance1_pool + amount_in
+    )
+
+    assert (
+        token0.balanceOf(pool_initialized_with_liquidity.address) == balance0_pool_after
+    )
+    assert (
+        token1.balanceOf(pool_initialized_with_liquidity.address) == balance1_pool_after
+    )
 
 
 @pytest.mark.parametrize("zero_for_one", [True, False])
@@ -97,8 +195,59 @@ def test_router_exact_input_single__returns_amount_out(
     alice,
     chain,
     zero_for_one,
+    sqrt_price_math_lib,
+    swap_math_lib,
 ):
-    pass
+    state = pool_initialized_with_liquidity.state()
+    fee = pool_initialized_with_liquidity.fee()
+
+    token_in = (
+        pool_initialized_with_liquidity.token0()
+        if zero_for_one
+        else pool_initialized_with_liquidity.token1()
+    )
+    token_out = (
+        pool_initialized_with_liquidity.token1()
+        if zero_for_one
+        else pool_initialized_with_liquidity.token0()
+    )
+    maintenance = pool_initialized_with_liquidity.maintenance()
+
+    deadline = chain.pending_timestamp + 3600
+    amount_out_min = 0
+    sqrt_price_limit_x96 = 0
+
+    (reserve0, reserve1) = calc_amounts_from_liquidity_sqrt_price_x96(
+        state.liquidity, state.sqrtPriceX96
+    )
+    amount_in = 1 * reserve0 // 100 if zero_for_one else 1 * reserve1 // 100
+
+    params = (
+        token_in,
+        token_out,
+        maintenance,
+        alice.address,  # recipient
+        deadline,
+        amount_in,
+        amount_out_min,
+        sqrt_price_limit_x96,
+    )
+    tx = router.exactInputSingle(params, sender=sender)
+
+    # calculate amount out
+    amount_in_less_fee = amount_in - swap_math_lib.swapFees(amount_in, fee)
+    sqrt_price_x96_next = sqrt_price_math_lib.sqrtPriceX96NextSwap(
+        state.liquidity,
+        state.sqrtPriceX96,
+        zero_for_one,
+        amount_in_less_fee,
+    )  # price change before fees added
+
+    (amount0, amount1) = swap_math_lib.swapAmounts(
+        state.liquidity, state.sqrtPriceX96, sqrt_price_x96_next
+    )
+    amount_out = -amount1 if zero_for_one else -amount0
+    assert tx.return_value == amount_out
 
 
 @pytest.mark.parametrize("zero_for_one", [True, False])
@@ -110,7 +259,42 @@ def test_router_exact_input_single__reverts_when_past_deadline(
     chain,
     zero_for_one,
 ):
-    pass
+    state = pool_initialized_with_liquidity.state()
+
+    token_in = (
+        pool_initialized_with_liquidity.token0()
+        if zero_for_one
+        else pool_initialized_with_liquidity.token1()
+    )
+    token_out = (
+        pool_initialized_with_liquidity.token1()
+        if zero_for_one
+        else pool_initialized_with_liquidity.token0()
+    )
+    maintenance = pool_initialized_with_liquidity.maintenance()
+
+    deadline = chain.pending_timestamp - 1
+    amount_out_min = 0
+    sqrt_price_limit_x96 = 0
+
+    (reserve0, reserve1) = calc_amounts_from_liquidity_sqrt_price_x96(
+        state.liquidity, state.sqrtPriceX96
+    )
+    amount_in = 1 * reserve0 // 100 if zero_for_one else 1 * reserve1 // 100
+
+    params = (
+        token_in,
+        token_out,
+        maintenance,
+        alice.address,  # recipient
+        deadline,
+        amount_in,
+        amount_out_min,
+        sqrt_price_limit_x96,
+    )
+
+    with reverts("Transaction too old"):
+        router.exactInputSingle(params, sender=sender)
 
 
 @pytest.mark.parametrize("zero_for_one", [True, False])
@@ -121,5 +305,57 @@ def test_router_exact_input_single__reverts_when_amount_out_less_than_min(
     alice,
     chain,
     zero_for_one,
+    sqrt_price_math_lib,
+    swap_math_lib,
 ):
-    pass
+    state = pool_initialized_with_liquidity.state()
+    fee = pool_initialized_with_liquidity.fee()
+
+    token_in = (
+        pool_initialized_with_liquidity.token0()
+        if zero_for_one
+        else pool_initialized_with_liquidity.token1()
+    )
+    token_out = (
+        pool_initialized_with_liquidity.token1()
+        if zero_for_one
+        else pool_initialized_with_liquidity.token0()
+    )
+    maintenance = pool_initialized_with_liquidity.maintenance()
+
+    deadline = chain.pending_timestamp + 3600
+    sqrt_price_limit_x96 = 0
+
+    (reserve0, reserve1) = calc_amounts_from_liquidity_sqrt_price_x96(
+        state.liquidity, state.sqrtPriceX96
+    )
+    amount_in = 1 * reserve0 // 100 if zero_for_one else 1 * reserve1 // 100
+
+    # calculate amount out
+    amount_in_less_fee = amount_in - swap_math_lib.swapFees(amount_in, fee)
+    sqrt_price_x96_next = sqrt_price_math_lib.sqrtPriceX96NextSwap(
+        state.liquidity,
+        state.sqrtPriceX96,
+        zero_for_one,
+        amount_in_less_fee,
+    )  # price change before fees added
+
+    (amount0, amount1) = swap_math_lib.swapAmounts(
+        state.liquidity, state.sqrtPriceX96, sqrt_price_x96_next
+    )
+    amount_out = -amount1 if zero_for_one else -amount0
+
+    amount_out_min = amount_out + 1
+    params = (
+        token_in,
+        token_out,
+        maintenance,
+        alice.address,  # recipient
+        deadline,
+        amount_in,
+        amount_out_min,
+        sqrt_price_limit_x96,
+    )
+
+    with reverts("Too little received"):
+        router.exactInputSingle(params, sender=sender)
