dc8cfe42d18429cc58face5feba7b9db77da1820
0xcivita <0xcivita@proton.me>
2023-11-26 17:06:13 -0500

    update deploy script for quoter

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -12,18 +12,19 @@ def main():
         if deployer_name != ""
         else accounts.test_accounts[0]
     )
-    click.echo(f"Init balance of deployer: {deployer.balance / 1e18} ETH")
+    click.echo(f"Deployer address: {deployer.address}")
+    click.echo(f"Deployer balance: {deployer.balance / 1e18} ETH")
 
-    factory_addr = click.prompt("Marginal v1 factory address", type=str)
-    weth9_addr = click.prompt("WETH9 address", type=str)
+    factory_address = click.prompt("Marginal v1 factory address", type=str)
+    weth9_address = click.prompt("WETH9 address", type=str)
     publish = click.prompt("Publish to Etherscan?", default=False)
 
     # deploy marginal v1 position manager
     if click.confirm("Deploy Marginal v1 NFT position manager?"):
         click.echo("Deploying Marginal v1 NFT position manager ...")
         manager = project.NonfungiblePositionManager.deploy(
-            factory_addr,
-            weth9_addr,
+            factory_address,
+            weth9_address,
             sender=deployer,
             publish=publish,
         )
@@ -33,9 +34,20 @@ def main():
     if click.confirm("Deploy Marginal v1 router?"):
         click.echo("Deploying Marginal v1 router ...")
         router = project.Router.deploy(
-            factory_addr,
-            weth9_addr,
+            factory_address,
+            weth9_address,
             sender=deployer,
             publish=publish,
         )
         click.echo(f"Deployed Marginal v1 router to {router.address}")
+
+    # deploy marginal v1 quoter
+    if click.confirm("Deploy Marginal v1 quoter?"):
+        click.echo("Deploying Marginal v1 quoter ...")
+        quoter = project.Quoter.deploy(
+            factory_address,
+            weth9_address,
+            sender=deployer,
+            publish=publish,
+        )
+        click.echo(f"Deployed Marginal v1 quoter to {quoter.address}")
